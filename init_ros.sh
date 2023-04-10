833ee3b4bf60800fc5e6f570dd1690b4cae8aac6
DinossauroBebado <guipsilva32@gmail.com>
2023-04-10 13:22:29 +0000

    inicia ROS ao ligar rasp

--- init_ros.sh
+++ init_ros.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ~/fred_scripts&&roslaunch ROS.launch &
+source /opt/ros/noetic/setup.bash&&source ~/catkin_ws/devel/setup.bash&&cd /home/ubuntu/fred_scripts&&roslaunch ROS.launch &
 
 
 cd&&cd fred_web_interface&&python3 servidor.py
