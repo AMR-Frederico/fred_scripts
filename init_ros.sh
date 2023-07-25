639021dd9e9d77d3856259cc9bec45b828dc4f99
LeticiaRP <leticiarp2000@hotmail.com>
2023-07-25 20:09:41 +0000

    modification

--- init_ros.sh
+++ init_ros.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-source /opt/ros/noetic/setup.bash&&source ~/catkin_ws/devel/setup.bash&&cd /home/ubuntu/fred_scripts&&roslaunch ROS.launch 
+source /opt/ros/noetic/setup.bash&&source ~/catkin_ws/devel/setup.bash&&cd ~/fred_scripts&&roslaunch ROS.launch 
 
 
 
