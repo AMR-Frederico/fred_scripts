224bd4545a723ae45a3ef163243c3af91afd2932
LeticiaRP <leticiarp2000@hotmail.com>
2023-09-07 00:53:10 -0300

    update fred_launch

--- init_ros.sh
+++ init_ros.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-source /opt/ros/noetic/setup.bash&&source ~/catkin_ws/devel/setup.bash&&cd ~/fred_scripts&&roslaunch ROS.launch 
+#source /opt/ros/noetic/setup.bash&&source ~/catkin_ws/devel/setup.bash&&cd ~/fred_scripts&&roslaunch ROS.launch 
 
-
+source /opt/ros/noetic/setup.bash&&source ~/catkin_ws/devel/setup.bash&&cd ~/fred_scripts&&roslaunch ROS.launch
 
 
